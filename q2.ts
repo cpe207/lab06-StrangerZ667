76e23190659a2601dcca70f72d3ff4966b8faaf1
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-08 19:05:33 +0700

    V1

--- q2.ts
+++ q2.ts
@@ -1,19 +1,31 @@
-// const axios = require("axios");
 import axios from "axios";
 
-/* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try{
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+      const Id = res.data
+      const resp = await axios.get(`https://jsonplaceholder.typicode.com/users/${Id.userId}`)
+      const intel = resp.data
+      const output = {
+            owner: intel.name,
+            title: Id.title,
+            completed: Id.completed,
+          }
+        return output
+      }
+  catch(err) {
+      return "INVALID TODO ID"
+     }
 };
 
-//test case
 const input1 = 15;
 const input2 = 60;
 const input3 = 250;
 
-//run
 getTodo(input1).then((result) => console.log(result));
 getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
 
 export default getTodo;
+
+//นายกรรชัย คุ้มดี 660610739
