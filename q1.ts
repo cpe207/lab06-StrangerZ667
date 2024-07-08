beac23955bd581d8068ab0177071f7a77b42a237
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-08 19:05:21 +0700

    V1

--- q1.ts
+++ q1.ts
@@ -1,18 +1,21 @@
-// const axios = require("axios");
 import axios from "axios";
 
-/* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
-  /* Your code here */
-};
+  try{
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+      return res.data.name
+    }
+  catch(err) {
+      return `INVALID USER ID`
+    }
+  }
 
-//test case
 const input1 = 1;
 const input2 = 100;
 
-//run
 getUser(input1).then((result) => console.log(result));
 getUser(input2).then((result) => console.log(result));
 
-// module.exports = getUser;
 export default getUser;
+
+//นายกรรชัย คุ้มดี 660610739
